410bfba77e0118d12781aced1e65f59021142cff
snyder2406 <shelbychsnyder@gmail.com>
2025-02-07 23:52:34 -0500

    Got shit done

--- map-your-degree/src/Pages/Login/Login.jsx
+++ map-your-degree/src/Pages/Login/Login.jsx
@@ -1,14 +1,31 @@
-import './Login.css'
+import './login.css';
 
-function Login() {
-    return (
-        <>
-        <div className="login-container">
-        <h1 className="login-header">Login</h1>
-        <button className="login-button">Create an Account</button>
-        <button className="login-button">Login</button>
-        </div></>
-    )
+const Login = () => {
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form>
+        <input 
+          type="text" 
+          placeholder="Username" 
+          name="username" 
+          required 
+        />
+        <input 
+          type="password" 
+          placeholder="Password" 
+          name="password" 
+          required 
+        />
+        <button type="submit" className="login-btn">
+          Login
+        </button>
+        <button type="button" className="create-account-btn">
+          <a href="/create-account">Create an Account</a>
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
