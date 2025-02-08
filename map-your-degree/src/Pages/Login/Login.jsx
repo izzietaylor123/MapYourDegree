9e0cf86bf343e14c11017e0ed36aa48c288f3f99
snyder2406 <shelbychsnyder@gmail.com>
2025-02-07 23:41:16 -0500

    Starting to build login page :)

---
+++ map-your-degree/src/Pages/Login/Login.jsx
@@ -0,0 +1,14 @@
+import './Login.css'
+
+function Login() {
+    return (
+        <>
+        <div className="login-container">
+        <h1 className="login-header">Login</h1>
+        <button className="login-button">Create an Account</button>
+        <button className="login-button">Login</button>
+        </div></>
+    )
+}
+
+export default Login
