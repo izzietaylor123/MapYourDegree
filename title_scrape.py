8d1268fc9158c548d3203170d150f9751d9ad4a8
Isabel Taylor <166870184+izzietaylor123@users.noreply.github.com>
2025-02-08 16:47:42 -0500

    Update title_scrape.py

--- title_scrape.py
+++ title_scrape.py
@@ -19,14 +19,8 @@ def get_title(url):
     degree = title_parts[1].split()[0].strip()  # Extracts "BSCS" (or equivalent)
     
 
-<<<<<<< HEAD
     # Print the extracted title
     return ("Website Title:", main_title)
-=======
-    # Concatenate and print the final result
-    final_title = f"{main_title}, {degree}"
-    print("Website Title:", final_title)
->>>>>>> e3426773cfd0d423c5a83be05646e4f8af102772
 
 # Pass the URL as an argument
 get_title("https://catalog.northeastern.edu/undergraduate/computer-information-science/computer-science/bscs/#ARIN")
