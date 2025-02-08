d088f2c249b4c92411bb54e8e6790421935cc826
Isabel Taylor <166870184+izzietaylor123@users.noreply.github.com>
2025-02-08 17:10:49 -0500

    trying to make streamlit change pages

--- pages/display_classes.py
+++ pages/display_classes.py
@@ -5,7 +5,6 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-
 word_to_number = {
 "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4,
 "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
@@ -14,26 +13,6 @@ word_to_number = {
 
 def parse_number(word):
     return word_to_number.get(word.lower(), "Invalid number")
-
-def get_title(url):
-    # Send HTTP request
-    response = requests.get(url)
-
-    # Parse HTML
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    # Extract the full title
-    title = soup.title.string
-
-    # Split the title to get the first part ("Computer Science")
-    title_parts = title.split("<")
-    main_title = title_parts[0].strip()  # Remove any leading/trailing spaces
-
-    # Print the extracted title
-    print(main_title)
-
-    # Print the extracted title
-    return (main_title)
 
 def get_courses(url, major, degree):
 
@@ -121,92 +100,82 @@ def get_courses(url, major, degree):
     return csv_list
 
 
-st.title("See Your Degree Track!")
-st.write("Please input your major url to continue.")
+url = st.session_state['url']
 
-url = st.text_input("Degree URL")
-if url:
+degree = st.session_state['degree']
 
-    degree = get_title(url)
+major_degree = degree.split(",")
+major = major_degree[0].strip()
+major.replace(" ", "-")
+degree = major_degree[1].strip()
 
-    st.write(f"**Is this your degree?** {degree}")
+csv_list = get_courses(url, major, degree)
+i = 0
+for file in csv_list:
+    i += 1
+    filepath = f"{file}"
+    df = pd.read_csv(filepath)
 
-    if st.button("Yes!"):
-        major_degree = degree.split(",")
-        major = major_degree[0].strip()
-        major.replace(" ", "-")
-        degree = major_degree[1].strip()
+
+
+    reqs_df = pd.read_csv(filepath)
+
+    # Display editable checkbox column
+    edited_df = st.data_editor(
+        reqs_df,
+        column_config={
+            "Completed": st.column_config.CheckboxColumn("Completed"),
+        },
+        use_container_width=True
+    )
+
+    # Button to save changes
+    if st.button("Save Changes", key = i):
+        i += 1
+        edited_df.to_csv(f"completed_{filepath}", index=False)
+        st.success("Changes saved successfully!")
         
-        csv_list = get_courses(url, major, degree)
-        i = 0
-        for file in csv_list:
-            i += 1
-            filepath = f"{file}"
-            df = pd.read_csv(filepath)
+        personalised_df = pd.read_csv(f"completed_{filepath}")
 
 
-
-            reqs_df = pd.read_csv(filepath)
-
-            # Display editable checkbox column
-            edited_df = st.data_editor(
-                reqs_df,
-                column_config={
-                    "Completed": st.column_config.CheckboxColumn("Completed"),
-                },
-                use_container_width=True
-            )
-
-            # Button to save changes
-            if st.button("Save Changes", key = i):
-                i += 1
-                edited_df.to_csv(f"completed_{filepath}", index=False)
-                st.success("Changes saved successfully!")
-                
-                personalised_df = pd.read_csv(f"completed_{filepath}")
-
-
-                num_required = 0
-                num_required_taken = 0
-                req_groups = {} # req_group : num required, satisfied
-                for row in personalised_df.itertuples():
-                    if row[5] == 'All':
-                        num_required += 1
-                        if row[6] == True:
-                            num_required_taken += 1
-                    elif row[4] not in req_groups:
-                        req_groups[row[4]] = [parse_number(row[5]), False]
-                        num_required += parse_number(row[5])
-                        if row[6] == True:
-                            num_required_taken += 1
-                            req_groups[row[4]][0] -=1
-                            if req_groups[row[4]][0] == 0:
-                                req_groups[row[4]][1] = True
-                    elif req_groups[row[4]][1] == False:
-                        if row[6] == True:
-                            num_required_taken += 1
-                            req_groups[row[4]][0] -=1
-                            if req_groups[row[4]][0] == 0:
-                                req_groups[row[4]][1] = True
+        num_required = 0
+        num_required_taken = 0
+        req_groups = {} # req_group : num required, satisfied
+        for row in personalised_df.itertuples():
+            if row[5] == 'All':
+                num_required += 1
+                if row[6] == True:
+                    num_required_taken += 1
+            elif row[4] not in req_groups:
+                req_groups[row[4]] = [parse_number(row[5]), False]
+                num_required += parse_number(row[5])
+                if row[6] == True:
+                    num_required_taken += 1
+                    req_groups[row[4]][0] -=1
+                    if req_groups[row[4]][0] == 0:
+                        req_groups[row[4]][1] = True
+            elif req_groups[row[4]][1] == False:
+                if row[6] == True:
+                    num_required_taken += 1
+                    req_groups[row[4]][0] -=1
+                    if req_groups[row[4]][0] == 0:
+                        req_groups[row[4]][1] = True
 
 
-                pct_taken = (num_required_taken/num_required)*100
-                all_req = [pct_taken, 100-pct_taken]
-                labels = ["Classes taken", "Classes left"]
+        pct_taken = (num_required_taken/num_required)*100
+        all_req = [pct_taken, 100-pct_taken]
+        labels = ["Classes taken", "Classes left"]
 
 
-                # Sample array data
+        # Sample array data
 
 
-                st.title("Percent of degree completed:")
+        st.title("Percent of degree completed:")
 
-                fig, ax = plt.subplots()
-                colors = ["#A52A2A", "#660000"]  # Light red, blue, green, and orange
+        fig, ax = plt.subplots()
+        colors = ["#A52A2A", "#660000"]  # Light red, blue, green, and orange
 
-                ax.pie(all_req, labels=labels, colors=colors,autopct="%1.1f%%", startangle=90)
-                ax.axis("equal")  # Equal aspect ratio ensures pie is drawn as a circle.
+        ax.pie(all_req, labels=labels, colors=colors,autopct="%1.1f%%", startangle=90)
+        ax.axis("equal")  # Equal aspect ratio ensures pie is drawn as a circle.
 
-                st.pyplot(fig)
-
-
-
+        st.pyplot(fig)
---
+++ pages/enter_degree.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import requests
+from bs4 import BeautifulSoup
+
+def get_title(url):
+    # Send HTTP request
+    response = requests.get(url)
+
+    # Parse HTML
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Extract the full title
+    title = soup.title.string
+
+    # Split the title to get the first part ("Computer Science")
+    title_parts = title.split("<")
+    main_title = title_parts[0].strip()  # Remove any leading/trailing spaces
+
+    # Print the extracted title
+    print(main_title)
+
+    # Print the extracted title
+    return (main_title)
+
+st.title("See Your Degree Track!")
+st.write("Please input your major url to continue.")
+
+url = st.text_input("Degree URL")
+if url:
+
+    degree = get_title(url)
+
+    st.write(f"**Is this your degree?** {degree}")
+
+    if st.button("Yes!"):
+        st.session_state['degree'] = degree,
+        st.session_state['url'] = url,
+        st.switch_page("pages/display_classes.py")
